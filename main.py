0ead62b0d881bf8ffa77d56d4a3f85933f2f3c7f
Alexandre C. Andreani <alexandre.c.andreani@gmail.com>
2018-05-08 09:59:24 -0300

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route("/bif")
+def index():
+    return render_template('bif.html')
+
+
+@app.route("/baker")
+def baker():
+    return render_template('baker.html')
+
+
+if __name__ == "__main__":
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, use_reloader=True)
